d4c27b40d85dad9d9402124ebd7a2973f22477e1
Niels Siskens <siskens@gmail.com>
2015-02-11 10:33:16 -0500

    completed it completely

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -27,17 +27,16 @@ class ProductsController < ApplicationController
 
   def update
   		@product = Product.find(params[:id])
-    	@product.update!(product_params)
-    	redirect_to product_path(@product)
+    	if @product.update!(product_params)
+    	 redirect_to root_path, notice: "Successfully Updated a Product"
+      end
   end
   
   def destroy
     @product = Product.find(params[:id])
-    if @product
-      @product.destroy
-      flash[:information] = t('I just destroyed #{@product(params [:id])}')
-    end
-  	  redirect_to root_url
+    @product.destroy
+    redirect_to root_url, notice: "Product Destruction Completed"
+
   end
   def product_params    
     params.require(:product).permit(:name,:description, :price, :published)
